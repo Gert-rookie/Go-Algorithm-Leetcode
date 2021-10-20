379d9722954ea6bd0647f5b921315b740ca306d3
honghaoteng <1071436942@qq.com>
2021-10-21 00:55:14 +0800

    chore:排序算法

---
+++ src/sort/bubblingSort.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+/**
+  冒泡排序
+*/
+func main() {
+	var a = []int{12, 20, 5, 16, 15, 1, 30, 45, 23, 9}
+	bubblingSort(a)
+	fmt.Println(a)
+}
+
+/**
+  冒泡排序
+*/
+func bubblingSort(a []int) {
+	if a == nil || len(a) < 2 {
+		return
+	}
+	for i := 0; i < len(a)-1; i++ {
+		for j := 0; j < len(a)-1-i; j++ {
+			if a[j] > a[j+1] {
+				a[j], a[j+1] = a[j+1], a[j]
+			}
+		}
+	}
+}
---
+++ src/sort/chooseSort.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+
+}
+
+/**
+  选择排序
+*/
+func chooseSort(a []int) {
+
+}
